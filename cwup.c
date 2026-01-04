b6e48dfdbdad6c4bdeebbc1e41b152cf5317a51c
Lexi <lexi@spifftech.uk>
2026-01-04 17:27:39 +0000

    Made it display a UV map

--- cwup.c
+++ cwup.c
@@ -72,8 +72,8 @@ int initialise(){
 	background = BlackPixel(dpy,screen_num);
 	border = WhitePixel(dpy, screen_num);
 
-	width = 40;
-	height = 40;
+	width = 512;
+	height = 512;
 
 	win = XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), 0, 0, width, height, 2, border, background);
 
@@ -94,6 +94,12 @@ int initialise(){
 	//}
 	return 0;
 }
+
+unsigned long _RGB(int r, int g, int b){
+	return b + (g<<8) + (r <<16);
+
+}
+
 int displayFrame(XFile *display_file){
 	XNextEvent(dpy, &ev);
 	switch(ev.type){
@@ -127,6 +133,13 @@ int main(int argc, char *argv[]){
 	for (int i = 0; i < 40; i++){
 		xfile_put_pixel(display_file, i, 20, WhitePixel(dpy, screen_num));
 
+	}
+	for (int x = 0; x < 512; x++){
+		for (int y = 0; y < 512;y++){
+			float u = (float)x / 512;
+			float v = (float)y / 512;
+			xfile_put_pixel(display_file, x, y, _RGB((int)(u*255),(int)(v*255),0));
+	}
 	}
 	while (1){
 		displayFrame(display_file);
