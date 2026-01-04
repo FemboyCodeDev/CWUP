f8589ff4810111fbcabbf2ffae6f4b364a7c8d4c
alex <femboy@spifftech.uk>
2026-01-04 23:52:00 +0000

    Added cwup3d.c todo list. No function implemented yet.

---
+++ cwup3d.c
@@ -0,0 +1,12 @@
+// TODO: Add vec2 structs
+// TODO: Add vec3 structs
+// TODO: Add vec3 -> vec2 projection functions
+// TODO: Add rotation functions
+
+// TODO: Add filled triangle renderer
+// TODO: Add filled quad renderer
+
+// TODO: Add texture tools
+// TODO: Add texture mapping tools
+// TODO: Add textured quad renderer
+
