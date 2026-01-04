7943a24f66feff216835ce71daecce28f69ff311
Lexi <lexi@spifftech.uk>
2026-01-04 14:44:00 +0000

    Started to write graphical engine

---
+++ cwup.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(int argc, char *argv[]){
+printf("Hello world");
+
+return 0;
+}
+
+
---
+++ readme.txt
@@ -0,0 +1,1 @@
+Controllable Window Utility Program
