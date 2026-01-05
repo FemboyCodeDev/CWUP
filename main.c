fd30b29968cd98f764f105ff11a40c5d2165627e
Lexi <lexi@spifftech.uk>
2026-01-05 16:09:26 +0000

    Now opens an openModel file and displays it

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include "cwup.c"
 #include "cwup3d.c"
 #include "openbmp.c"
+#include "openModel.h"
+
+
 int main(int argc, char *argv[]){
 
 	
@@ -18,9 +21,12 @@ int main(int argc, char *argv[]){
 		return 0;
 	}
 
+	openModel model = loadModel("test.openModel");
+	camera Cam;
+	Cam.sensorDistance = 0.1;
 
         initialise();
-	xfile_auto_flush(false);
+	//xfile_auto_flush(false);
         XFile *display_file = xfile_open(dpy, win, pen, width, height);
         for (int i = 0; i < 40; i++){
                 xfile_put_pixel(display_file, i, 20, WhitePixel(dpy, screen_num));
@@ -32,22 +38,28 @@ int main(int argc, char *argv[]){
                         float u = (float)x / 512;
                         float v = (float)y / 512;
 			vec2 p = {u,v};
-			vec2 a = {0.1,0.1};
-			vec2 b = {0.1,0.5};
-			vec2 c = {0.5,0.5};
-			vec2 d = {0.5, 0.1};
-			vec2 a1 = {0,0};
-			vec2 b1 = {0,1.0};
-			vec2 c1 = {1.0,1.0};
-			bool in_tri = triangle2d(a,b,c,p);
-			if (in_tri){
-				vec2 texCoord = distort_point(p,a,b,c,a1,b1,c1);
-				//printf("LoadingPixel");
-				openPixel *pix = getPixel(texture, texCoord.x*(texture->width), texCoord.y*(texture->height));
-				//printf("PixelLoaded");
-				xfile_put_pixel(display_file, x, y, _RGB(pix->r,pix->g,pix->b));
-                        	//xfile_put_pixel(display_file, x, y, _RGB((int)(u*255),(int)(v*255),0));
-        		}
+			for (int i = 0; i < model.triCount; i++){
+				Tri3 current_tri = model.triangles[i];
+				vec3 a3d = {current_tri.a.x,current_tri.a.y,current_tri.a.z};
+				vec3 b3d = {current_tri.b.x,current_tri.b.y,current_tri.b.z};
+				vec3 c3d = {current_tri.c.x,current_tri.c.y,current_tri.c.z};
+				vec2 a = project_3d(a3d,Cam);
+				vec2 b = project_3d(b3d,Cam);
+				vec2 c = project_3d(c3d,Cam);
+				//vec2 d = {0.5, 0.1};
+				vec2 a1 = {current_tri.uv.a.x,current_tri.uv.a.y};
+				vec2 b1 = {current_tri.uv.b.x,current_tri.uv.b.y};
+				vec2 c1 = {current_tri.uv.c.x,current_tri.uv.c.y};
+				bool in_tri = triangle2d(a,b,c,p);
+				if (in_tri){
+					vec2 texCoord = distort_point(p,a,b,c,a1,b1,c1);
+					//printf("LoadingPixel");
+					openPixel *pix = getPixel(texture, texCoord.x*(texture->width), texCoord.y*(texture->height));
+					//printf("PixelLoaded");
+					//xfile_put_pixel(display_file, x, y, _RGB(pix->r,pix->g,pix->b));
+                        		xfile_put_pixel(display_file, x, y, _RGB((int)(texCoord.x*255),(int)(texCoord.y*255),0));
+        			}
+			}
 	}
         }
         while (1){
---
+++ openModel.h
@@ -0,0 +1,118 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+typedef struct {
+	float x;
+	float y;
+	float z;
+
+} Point3;
+
+typedef struct {
+	float x;
+	float y;
+} Point2;
+
+typedef struct {
+	float x;
+	float y;
+} UVCoord;
+
+typedef struct {
+        UVCoord a;
+        UVCoord b;
+        UVCoord c;
+} UVTri2;
+
+typedef struct {
+	Point3 a;
+	Point3 b;
+	Point3 c;
+	UVTri2 uv;
+
+} Tri3;
+
+typedef struct {
+	int triCount;
+	Tri3 *triangles;
+} openModel;
+
+
+
+openModel loadModel(char filename[]){
+	FILE * fp;
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	openModel model;
+	//printf(filename);
+	fp = fopen(filename, "r");
+	if (fp == NULL){return model;}
+
+
+	int capacity = 2;
+	int count = 0;
+	model.triangles = malloc(capacity * sizeof(Tri3));
+
+	bool eof = false;
+	while ((read == getline(&line, &len, fp)) != -1 && !eof){
+		//printf("Got line of length %zu:\n", read);
+		if (strlen(line) > 2){
+			if (strstr(line, "#") == NULL){
+				//printf("%s",line);
+				if (strstr(line, "eof") != NULL) {eof = true;} else{
+					printf("%s",line);
+					if (count >= capacity) {
+						capacity *= 2;
+						model.triangles = realloc(model.triangles, capacity * sizeof(Tri3));
+					}
+					float results[15];
+					int num_values = openModel_parse_triangle(line,results);
+					//printf("%d",num_values);
+					if (num_values > 0){
+						Point3 a = {results[0],results[1],results[2]};
+						Point3 b = {results[3],results[4],results[5]};
+						Point3 c = {results[6],results[7],results[8]};
+						UVCoord uv1 = {results[9],results[10]};
+						UVCoord uv2 = {results[11],results[12]};
+						UVCoord uv3 = {results[13],results[14]};
+						UVTri2 uv_tri = {uv1,uv2,uv3};
+						Tri3 new_tri = {a,b,c,uv_tri};
+						model.triangles[count] = new_tri;
+						count++;
+						model.triCount ++;
+					}
+				}
+			}
+		}
+	}
+	//printf("%d\n",count);
+	model.triCount = count;
+	fclose(fp);
+	return model;
+}
+
+
+
+
+int openModel_parse_triangle(char *input, float *output) {
+	char *token;
+	int count = 0;
+	// Get the first token (the shape type)
+	token = strtok(input, "; ");
+
+	// Check if it's a "tri"
+	if (token == NULL || strcmp(token, "tri") != 0) {
+        return -1; 
+	}
+	// Continue parsing the rest of the numbers
+	// Delimiters include semicolon, comma, and space
+	while ((token = strtok(NULL, ";, ")) != NULL) {
+		output[count++] = atof(token);
+	}
+	return count;
+}
